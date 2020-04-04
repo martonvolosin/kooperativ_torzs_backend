1366b1683b44bd375a66ebd2b7774d71d845c504
Volosin Márton <volosin.marton@gmail.com>
2020-04-04 17:20:29 +0200

    typos fixed

--- functions/validation/index.js
+++ functions/validation/index.js
@@ -4,7 +4,7 @@ exports.updateUser = require('./user/updateUserSchema');
 exports.addItem = require('./item/addItemSchema');
 
 // GET
-exports.getUser = require('./user/getUserSchema');
+
 
 // DELETE
-exports.deleteUser = require('./user/deleteUserSchema');
+exports.deletItem = require('./item/deleteItemSchema');
