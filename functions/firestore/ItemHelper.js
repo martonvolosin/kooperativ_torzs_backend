43d75d0f7d997d4bc314bacf4b1813eb6469336b
attila.ujvarosy <attila.ujvarosy@gmail.com>
2020-04-04 18:22:35 +0200

    addItem function added

--- functions/firestore/ItemHelper.js
+++ functions/firestore/ItemHelper.js
@@ -1,9 +1,11 @@
+const { REFS } = require('../utils/constants');
+
+const itemsRef = REFS.DB.collection('items');
+
 module.exports.createItem = body => new Promise(async (resolve, reject) => {
-  const {
-    name
-  } = body;
   try {
-    console.log('INIT')
+    console.log('Creating new item...')
+    await itemsRef.add(body);
     resolve();
   } catch (error) {
     reject(new Error(error.message));
--- functions/https/item/addItem.f.js
+++ functions/https/item/addItem.f.js
@@ -3,8 +3,11 @@ const { STATUS_CODES } = require('../../utils/constants');
 
 // eslint-disable-next-line
 const addItem = async (req, res, next) => {
+  const { body } = req;
+  body.userId = req.user.id;
+  
   try {
-    await createUser(req.body, res);
+    await createItem(body, res);
     res.send({ status: 201, message: STATUS_CODES.STATUS_201 });
   } catch (error) {
     return next(error);
