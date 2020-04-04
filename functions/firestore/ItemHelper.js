7d8f6e07606548ea19e13d165fa81966dfdc5764
attila.ujvarosy <attila.ujvarosy@gmail.com>
2020-04-04 19:05:06 +0200

    Return new item id

--- functions/firestore/ItemHelper.js
+++ functions/firestore/ItemHelper.js
@@ -3,8 +3,8 @@ const { REFS } = require('../utils/constants');
 module.exports.createItem = body => new Promise(async (resolve, reject) => {
   try {
     console.log('Creating new item...')
-    await REFS.COLLECTIONS.ITEMS.add(body);
-    resolve();
+    const result = await REFS.COLLECTIONS.ITEMS.add(body);
+    resolve(result.id);
   } catch (error) {
     reject(new Error(error.message));
   }
